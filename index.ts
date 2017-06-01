4bfbc3c12b79d21bc2d2bbcc22ca6a5867563911
Vlad Pakin <pakinvlad@gmail.com>
2017-06-01 18:35:46 +0300

    refactoring and add new methods

--- index.ts
+++ index.ts
@@ -17,7 +17,8 @@ let linkedList = new LinkedList(),
     nastya = new Person("Nastya", 24),
     sasha = new Person("Sasha", 39),
     andrey = new Person("Andrey", 39),
-    dima = new Person("Dima", 39);
+    dima = new Person("Dima", 39),
+    sam = new Person("Sam", 25);
 
 
 linkedList.push(vlad);
@@ -25,3 +26,19 @@ linkedList.push(nastya);
 linkedList.push(sasha);
 linkedList.push(andrey);
 linkedList.push(dima);
+
+console.log("get by index");
+console.log(linkedList.getByIndex(0));
+
+console.log("\n");
+linkedList.forEach((person: Person) => console.log(person));
+
+linkedList.insert(sam, 3);
+
+console.log("\n");
+linkedList.forEach((person: Person) => console.log(person));
+
+linkedList.remove(1);
+
+console.log("\n");
+linkedList.forEach((person: Person) => console.log(person));
--- lib/linked-list.ts
+++ lib/linked-list.ts
@@ -10,19 +10,22 @@ export class LinkedList<T> {
     constructor() {
     }
 
-    get first(): Node<T> {
-        return this._head;
+    get first(): T {
+        return this._head.data;
     }
 
-    get last(): Node<T> {
-        return this._tail;
+    get last(): T {
+        return this._tail.data;
     }
 
     get length(): number {
         return this._count;
     }
 
-    public push(data: T) {
+    /**
+     * Work as Javascript`s Array.push() method;
+     */
+    public push(data: T): void {
         let node: Node<T> = new Node(data);
 
         if (this._head === null) {
@@ -35,8 +38,10 @@ export class LinkedList<T> {
         this._count++;
     }
 
-
-    public pushAsFirst(data: T) {
+    /**
+     * Work as Javascript`s Array.unshift() method;
+     */
+    public unshift(data: T): void {
         let node: Node<T> = new Node(data);
 
         node.next = this._head;
@@ -48,70 +53,149 @@ export class LinkedList<T> {
         this._count++;
     }
 
-    public remove(data: T) {
-        let current: Node<T> = this._head;
-        let previous: Node<T> = null;
+    /**
+     * Work as Javascript`s Array.shift() method;
+     * @returns {T}
+     */
+    public shift(): T {
+        if (this._head === null) return null;
 
-        while (current !== null) {
-
-            if (compareData(current.data, data)) {
-
-                if (previous === null) {
-                    this._head = this._head.next;
-
-                    if (this._head === null) {
-                        this._tail = null;
-                    }
-
-                } else {
-                    previous.next = current.next;
-
-                    if (current.next === null) {
-                        this._tail = previous;
-                    }
-
-                }
-
-                this._count--;
-                return true;
-            }
-
-            previous = current;
-            current = current.next;
-        }
-
-        return false;
+        let previous: Node<T> = this._head;
+        this._head = this._head.next;
+        this._count--;
+        return previous.data;
     }
 
-    public contains(data: T) {
-        let current: Node<T> = this._head;
+    /**
+     * Work as Javascript`s Array.pop() method;
+     * @returns {T}
+     */
+    public pop(): T {
+        if (this._head === null) return null;
 
-        while (current !== null) {
-            if (compareData(current.data, data)) {
-                return true;
-            }
+        let beforeTail: Node<T> = this.getLastButOne(),
+            last: T = null;
 
-            current = current.next;
+        if (beforeTail === null) {
+            last = this._head.data;
+            this._head = null;
+        } else {
+            last = this._tail.data;
+            beforeTail.next = null;
+            this._tail = beforeTail;
         }
-        return false;
+
+
+        this._count--;
+        return last
     }
 
-    public getByIndex(index: number) {
-        if (typeof index !== "number") throw new TypeError("Index should be a number");
-        if (index > this._count || index < 0) return null;
+    public insert(data: T, index: number): void {
 
-        let counter = 0;
-        let current: Node<T> = this._head;
+        if (index === 0) {
+            this.unshift(data);
+            return;
+        }
+
+        if (index >= this._count) {
+            this.push(data);
+            return;
+        }
+
+        let
+            node: Node<T> = new Node(data),
+            curr: Node<T> = this._head,
+            prev: Node<T> = this._head,
+            counter: number = 0;
 
         while (counter < index) {
+            prev = curr;
+            curr = curr.next;
+            counter++;
+        }
+
+        node.next = prev.next;
+        prev.next = node;
+        this._count++;
+    }
+
+    /**
+     * Remove Node by input data.
+     * Compares data with all Nodes until found same and then removes it from list
+     * @param index : number
+     * @returns {boolean}
+     */
+    public remove(index: number): void {
+
+        if (index === 0) {
+            this.shift();
+            return;
+        }
+
+        if (index > this._count) {
+            this.pop();
+            return;
+        }
+
+        let current: Node<T> = this._head,
+            previous: Node<T> = null,
+            counter: number = 0;
+
+        while (counter < index) {
+            previous = current;
             current = current.next;
             counter++;
         }
 
-        return current;
+        previous.next = current.next;
+        this._count--;
     }
 
+    /**
+     * Compare input data with all data inside list and return compare result as a boolean;
+     * @param data : T
+     * @returns {boolean}
+     */
+    public contains(data: T): boolean {
+        let current: Node<T> = this._head;
+
+        while (current !== null) {
+            if (compareData(current.data, data)) {
+                return true;
+            }
+
+            current = current.next;
+        }
+        return false;
+    }
+
+    /**
+     * Get data by input index
+     *
+     * @param index
+     * @returns { data : T}
+     *
+     */
+    public getByIndex(index: number): T {
+        if (typeof index !== "number") throw new TypeError("Index should be a number");
+        if (index > this._count || index < 0) return null;
+        return this.findNodeByIndex(index).data;
+    }
+
+    /**
+     * Loop through whole list and call callback.
+     * @param callback
+     *
+     * Example:
+     *
+     * let list = new LinkedList()
+     * list.forEach((data : T) => console.log(data))
+     *
+     */
     public forEach(callback: (T) => void): void {
+
+        if (this._count <= 0) return null;
+
         let current: Node<T> = this._head;
 
         while (current !== null) {
@@ -140,6 +224,45 @@ export class LinkedList<T> {
             yield current.data;
             current = current.next;
         }
+    }
+
+
+    /**
+     * Helper.
+     *
+     * Loop through all item until found item by index
+     *
+     * @param index : number
+     * @returns {Node<T>}
+     */
+    private findNodeByIndex(index: number): Node<T> {
+        let counter = 0,
+            current: Node<T> = this._head;
+
+        while (counter < index) {
+            current = current.next;
+            counter++;
+        }
+
+        return current;
+    }
+
+    /**
+     * Take node in front of tail
+     * @returns {Node<T>}
+     */
+    private getLastButOne(): Node<T> {
+        let node: Node<T> = this._head;
+
+        if (node === null || node.next === null) {
+            return null;
+        }
+
+        while (node.next.next) {
+            node = node.next;
+        }
+
+        return node;
     }
 
 
